8db746796a24a4bf14b5d043e6d79cc415445e55
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2019-10-01 16:07:29 +0200

    Toke

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,7 +1,7 @@
+echo GH_TOKEN=$GH_TOKEN
+
 REMOTE="https://${GH_TOKEN}@github.com/janvanmansum/test-travis-mkdocs"
 
-git config --global user.name "${GH_NAME}"
-git config --global user.email "${GH_EMAIL}"
 git remote set-url origin ${REMOTE}
 
 mkdocs gh-deploy --force
